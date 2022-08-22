7ab5aa35b1601ef63e15448bf38c96ca7437d75a
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-08-22 03:54:21 +0200

    load conditional message

--- src/blogLists.js
+++ src/blogLists.js
@@ -1,12 +1,9 @@
 // props is used for reuseable codes, It is a way of passing parent component to a child component
 
-const BlogList = (props) => {
-    const blogs = props.blogs;
-    const title = props.title;
-
-    console.log(props, blogs);
+const BlogList = ({blogs, title}) => { 
     return ( 
         <div className="blog-list">
+        <h2>{ title }</h2>
         { blogs.map((blog) =>(
             <div className='blog-preview' key={blog.id}>
                 <h2> {blog.title} </h2>
--- src/home.js
+++ src/home.js
@@ -3,6 +3,7 @@ import BlogList from './BlogLists';
 
 const Home = () => {
      const [blogs, setBlogs] = useState(null);
+     const [isPending, setIsPending] = useState(true);
 
         /*{const handleDelete = (id) => {
             const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -10,17 +11,21 @@ const Home = () => {
         }}*/
 
         useEffect(() =>{
-            fetch('http://localhost:8000/blogs')
+            setTimeout(() =>{
+                 fetch('http://localhost:8000/blogs')
             .then(res =>{
                 return res.json()
             })
             .then(data =>{
                 console.log(data);
-                setBlogs(data)
+                setBlogs(data);
+                setIsPending(false);
             })
+            }, 500);
         }, []);
     return (
         <div className="home">
+            { isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
     );
