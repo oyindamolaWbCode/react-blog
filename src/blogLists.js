96f0a2eb9a52514cc98a90a4640e03502a490dfe
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-08-23 23:34:38 +0200

    database

--- src/blogLists.js
+++ src/blogLists.js
@@ -1,18 +1,18 @@
 // props is used for reuseable codes, It is a way of passing parent component to a child component
 
-const BlogList = ({blogs, title}) => { 
-    return ( 
-        <div className="blog-list">
-        <h2>{ title }</h2>
-        { blogs.map((blog) =>(
-            <div className='blog-preview' key={blog.id}>
-                <h2> {blog.title} </h2>
-                <p>Written by {blog.author} </p>
-                <p> {blog.year} </p>
-            </div>
-        ))}
+const BlogList = ({ blogs, title }) => {
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2> {blog.title} </h2>
+          <p>Written by {blog.author} </p>
+          <p> {blog.year} </p>
+        </div>
+      ))}
     </div>
-);
-} 
- 
+  );
+};
+
 export default BlogList;
--- src/home.js
+++ src/home.js
@@ -1,20 +1,24 @@
-import { useState, useEffect } from 'react';
-import BlogList from './BlogLists';
-import useFetch from './useFetch';
+import { useState, useEffect } from "react";
+import BlogList from "./BlogLists";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
-        /*{const handleDelete = (id) => {
+  /*{const handleDelete = (id) => {
             const newBlogs = blogs.filter(blog => blog.id !== id);
             setBlogs(newBlogs);
         }}*/
 
-    return (
-        <div className="home">
-            { isPending && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
-        </div>
-    );
-}    
+  return (
+    <div className="home">
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+    </div>
+  );
+};
 export default Home;
